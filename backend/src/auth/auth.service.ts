72a67c70e6f27db5c085def1004fd9c5463c5386
faeze dalili <faezedalili@gmail.com>
2026-06-28 23:30:23 +0330

    feat(auth): hash user password with bcrypt

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  register(registerDto: RegisterDto) {
+  async register(registerDto: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
     return {
-      message: 'Register API works',
-      registerDto,
+      message: 'password hash successfylly',
+      data: { ...registerDto, password: hashedPassword },
     };
   }
 }
